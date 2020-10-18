dff004b1a6cd5c293a2d2e68e6e24913593acbcd
Michael Kündig <michael.kuendig@gmail.com>
2020-10-19 00:12:44 +0200

    Update postgresql username

--- schema.sql
+++
@@ -1,7 +0,0 @@
-create table logo_detection
-(
-    id serial           not null primary key,
-    logo_name           varchar not null,
-    channel             varchar not null,
-    "timestamp"         timestamp not null
-);
